7495cddf45d6e7a16dc0d0209a741dd027330f5e
Pham Nguyen Hung <97870091+HangenYuu@users.noreply.github.com>
2024-03-25 14:17:37 +0000

    added terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,37 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "/workspaces/mage-zoomcamp/dezoomcamp-416702-ca161cc2bef0.json"
+  #ex: if you have a directory where this file is called keys with your service account json file
+  #saved there as my-creds.json you could use default = "./keys/my-creds.json"
+}
+
+
+variable "project" {
+  description = "Project"
+  default     = "dezoomcamp-416702"
+}
+
+variable "region" {
+  description = "Region"
+  default     = "us-central1"
+}
+
+variable "location" {
+  description = "Project Location"
+  default     = "US"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  default     = "nytime"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  default     = "dezoomcamp-416702-terra-bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
